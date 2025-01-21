30efd2f62a6681de85b0599e8f7509c6b9ebe67c
Tirth Chankeshawara <tirthchankeshwara@gmail.com>
2025-01-21 11:04:41 +0530

    updated HTML

--- Community Volunteering platform/CommunityVolunteering/Communityplatform/src/main/java/com/Volunteering_platform/Communityplatform/CommunityplatformApplication.java
+++ Community Volunteering platform/CommunityVolunteering/Communityplatform/src/main/java/com/Volunteering_platform/Communityplatform/CommunityplatformApplication.java
@@ -3,6 +3,7 @@ package com.Volunteering_platform.Communityplatform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CommunityplatformApplication {
 	public static void main(String[] args) {
--- Community Volunteering platform/CommunityVolunteering/Communityplatform/src/main/java/com/Volunteering_platform/Communityplatform/controller/HomeController.java
+++ Community Volunteering platform/CommunityVolunteering/Communityplatform/src/main/java/com/Volunteering_platform/Communityplatform/controller/HomeController.java
@@ -1,7 +1,9 @@
 package com.Volunteering_platform.Communityplatform.controller;
 
+import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class HomeController {
@@ -26,10 +28,15 @@ public class HomeController {
     {
         return "signup";
     }
-    @GetMapping("/getinvolved")
+    @GetMapping("/get-involved")
     public  String getinvolved()
     {
         return "get-involved";
+    }
+    @GetMapping("/search")
+    public String search(Model model) {
+        // Add logic here if needed (e.g., pass search data to the view)
+        return "search"; // Render the search.html page
     }
 
 }
